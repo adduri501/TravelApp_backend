8c2face693074e7813445c3182e86ce05f432bec
Adduri Praveen <>
2026-04-11 18:20:17 +0530

    removed

--- src/app/orm/models/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from app.orm.models.user_model import *
-from app.orm.models.otp_model import *
-from app.orm.models.device_model import *
-from app.orm.models.refresh_token_model import *
-from app.orm.models.passenger_model import *
-from app.orm.models.driver_model import *
--- src/run_server.py
+++
@@ -1,4 +0,0 @@
-import uvicorn
-
-if __name__=="__main__":
-    uvicorn.run("app.app_definition:travel_agency_api",host="127.0.0.1",port=9000,reload=True)
